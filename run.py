5eecd5da9bab4b15eaa744e0239d49e051af02e0
Valentin Schneider <vschneid@redhat.com>
2023-12-20 17:05:44 +0100

    run.py: Add support for extra cmdine arguments (--cmdline)

--- run.py
+++ run.py
@@ -1,25 +1,55 @@
 #!/usr/bin/env python3
 
+import re
 from argparse import ArgumentParser
 from subprocess import Popen
+
+RE_CMDLINE_APPEND = re.compile(r"-append '.+'")
+
+def iter_lines(filename):
+    with open(filename, "r") as fh:
+        for line in fh:
+            if line.startswith("#"):
+                continue
+
+            yield line.strip().strip("\\")
+
+def print_wrap(txt):
+    s = 0
+    for e in range(0, len(txt), 120):
+        e+=120
+        print("{}{}".format(txt[s:e], "" if e >= len(txt) else "\\"))
+        s = e
+
 
 def main():
     parser = ArgumentParser()
     parser.add_argument("topology", help="The NUMA topology file to use")
     parser.add_argument("--script", default="qemu.sh", help="The QEMU script to run")
+    parser.add_argument("--cmdline", default="", help="Extra cmdline arguments")
 
     args = parser.parse_args()
 
     topology_file = args.topology
     script_file = args.script
+    cmdline = args.cmdline
 
-    with open(topology_file, "r") as fh:
-        topology = " ".join([line.strip().strip("\\") for line in fh if not line.startswith("#")])
+    topology = " ".join(line for line in iter_lines(topology_file))
 
-    with open(script_file, "r") as fh:
-        script = " ".join([line.strip().strip("\\") for line in fh if not line.startswith("#")])
+    script_lines = []
+    for line in iter_lines(script_file):
+        if cmdline and RE_CMDLINE_APPEND.match(line):
+            # Index of last quote
+            idx = len(line) - 1 - line[::-1].find("'")
+            line = line[0:idx] + " {}'".format(cmdline)
+
+        script_lines.append(line)
+
+
+    script = " ".join(script_lines)
 
     cmd = "{} {}".format(script, topology)
+    print_wrap(cmd)
 
     with Popen(cmd, shell=True) as proc:
         proc.wait()
